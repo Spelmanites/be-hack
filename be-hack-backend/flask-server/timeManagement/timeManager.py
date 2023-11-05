f4e5ce980b6ea009c571cb3863c347834bba3c22
Nia Bronson <nia@Nias-MacBook-Air-5.local>
2023-11-05 04:39:55 -0600

    Fixed

--- be-hack-backend/flask-server/timeManagement/timeManager.py
+++ be-hack-backend/flask-server/timeManagement/timeManager.py
@@ -15,41 +15,6 @@ from flightAPIHandler import jsonConversion
 # ISO 8601 Format: YYYY-MM-DD Hour:Minutes:Seconds.Milliseconds
 # """
 
-
-def extractTime(keyVal):
-    """
-    This function will extract the hour, minutes, and second values from the departure time
-    """
-    departureTime = jsonConversion()["departureTime"]
-    #String slicing! Python is so good for this feature 
-    year = departureTime[0:3]
-    month = departureTime[5:6]
-    day = departureTime[8:9]
-    hour = departureTime[11:12]
-    minutes = departureTime[14:15]
-    seconds = departureTime[17:18]
-
-    key = ""
-    #Return value based on key
-    if (key == "hour"):
-        return hour
-    elif (key == "min"):
-        return minutes
-    elif (key == "sec"):
-        return seconds
-    elif (key == "day"):
-        return day
-    elif (key == "month"):
-        return month
-    elif (key == "year"):
-        return year
-    else:
-        print("Invalid key value")
-    
-    #Test Statements
-    print(hour, " ", minutes, " " , seconds, " " ,departureTime)
-    return hour
-
 def calcTime():
     """
     This function will calculate the time until departure using 
