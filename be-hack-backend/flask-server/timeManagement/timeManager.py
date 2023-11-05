be408f7874e9dfbbd54ec0af5d14e46ed1e97344
Nia Bronson <nia@Nias-MacBook-Air-5.local>
2023-11-05 04:39:20 -0600

    Kinda Buggy time method. Will be complete

--- be-hack-backend/flask-server/timeManagement/timeManager.py
+++ be-hack-backend/flask-server/timeManagement/timeManager.py
@@ -1,9 +1,10 @@
 
-# import datetime, timedelta
-# #Adding the path to the aaAPIHandling folder to access the jsonConversion function
-# import sys
-# sys.path.append('/Users/nia/Desktop/Code/Projects/BeSmart/be-hack/be-hack-backend/flask-server/aaAPIHandling')
-# from flightAPIHandler import jsonConversion
+from datetime import datetime
+
+#Adding the path to the aaAPIHandling folder to access the jsonConversion function
+import sys
+sys.path.append('/Users/nia/Desktop/Code/Projects/BeSmart/be-hack/be-hack-backend/flask-server/aaAPIHandling')
+from flightAPIHandler import jsonConversion
 
 # """
 # File Purpose:
@@ -15,29 +16,79 @@
 # """
 
 
-# def extractHour():
-#     """
-#     This function will extract the hour value from the departure time
-#     """
-#     departureTime = jsonConversion()["departureTime"]
-#     hour = departureTime[11:16]
+def extractTime(keyVal):
+    """
+    This function will extract the hour, minutes, and second values from the departure time
+    """
+    departureTime = jsonConversion()["departureTime"]
+    #String slicing! Python is so good for this feature 
+    year = departureTime[0:3]
+    month = departureTime[5:6]
+    day = departureTime[8:9]
+    hour = departureTime[11:12]
+    minutes = departureTime[14:15]
+    seconds = departureTime[17:18]
 
-#     print(hour, " ", departureTime)
-#     return hour
+    key = ""
+    #Return value based on key
+    if (key == "hour"):
+        return hour
+    elif (key == "min"):
+        return minutes
+    elif (key == "sec"):
+        return seconds
+    elif (key == "day"):
+        return day
+    elif (key == "month"):
+        return month
+    elif (key == "year"):
+        return year
+    else:
+        print("Invalid key value")
+    
+    #Test Statements
+    print(hour, " ", minutes, " " , seconds, " " ,departureTime)
+    return hour
 
-# def calcTime():
-#     """
-#     This function will calculate the time until departure using 
-#     the hours value from the current time and departure time
-#     """
-#     departureHour = extractHour()
-#     currHour = datetime.datetime.now().hour
-#     print(currHour, " ", depHour)
-#     timeUntilDeparture = depHour - currHour
-#     print(timeUntilDeparture)
+def calcTime():
+    """
+    This function will calculate the time until departure using 
+    the hours value from the current time and departure time
+    """    
 
-#     days = time_until_flight.days
-#     hours, seconds = divmod(time_until_flight.seconds, 3600)
-#     minutes, seconds = divmod(seconds, 60)
+    departureTime = jsonConversion()["departureTime"]
+    #String slicing! Python is so good for this feature 
+    year = departureTime[0:3]
+    month = departureTime[5:6]
+    day = departureTime[8:9]
+    hour = departureTime[11:12]
+    minutes = departureTime[14:15]
+    seconds = departureTime[17:18]
 
-# extractHour()
+    #Get Current Time
+    currYear = datetime.now().year
+    currMonth = datetime.now().month
+    currDays = datetime.now().day
+    currHour = datetime.now().hour
+    currMin = datetime.now().minute
+    currSec = datetime.now().second
+
+    currTime = datetime.utcnow()
+
+    #Calculate time until departure
+    departureTime = datetime(
+    int(year),
+    int(month),
+    int(day),
+    int(hour),
+    int(minutes),
+    int(seconds)
+)
+    timeUntilFlight = departureTime - currTime
+
+    #test statements
+    print(timeUntilFlight)
+
+    return timeUntilFlight
+
+calcTime()
